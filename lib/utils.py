83535ba5dd833909f95ed7a65e0701bafbadf376
Vignesh <vsomnath@student.ethz.ch>
2018-09-14 00:33:53 +0200

    Tweaks to multi-cell input generation

--- lib/utils.py
+++ lib/utils.py
@@ -3,10 +3,8 @@ import tensorflow as tf
 import numpy as np
 import os
 import json
-import matplotlib
-matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
-from scipy.stats import ks_2samp
 
 xav_init = tf.contrib.layers.xavier_initializer
 normal_init = tf.truncated_normal_initializer
@@ -114,26 +112,7 @@ def build_gaussian_training_set(num_subpopulations, num_cells, weights_subpopula
     return data, y_sub_populations
 
 
-def generate_random_subset(inputs, num_cells_per_input, batch_size):
-
-    """
-    Returns a random subset from input data of shape (batch_size, num_cells_per_input, num_markers)
-    :param inputs: numpy array, the input ndarray to sample from
-    :param num_cells_per_input: int, number of cells per multi-cell input
-    :param batch_size: int, batch size of the subset
-    :return:
-    """
-
-    num_cells_total = inputs.shape[0]
-    indices = [np.random.choice(range(num_cells_total), num_cells_per_input, replace=False)
-               for i in range(batch_size)]
-
-    subset = [inputs[index, ] for index in indices]
-
-    return np.array(subset), indices
-
-
-def get_batches(inputs, batch_size, num_batches, num_cells_per_input):
+def get_batches(inputs, batch_size, num_batches, num_cells_per_input, weights=None):
 
     """
     Generate multiple batches of training data from given inputs
@@ -141,70 +120,44 @@ def get_batches(inputs, batch_size, num_batches, num_cells_per_input):
     :param batch_size: int, batch_size of each batch generated
     :param num_batches: int, number of batches of training data to generate
     :param num_cells_per_input: int, number of cells per multi-cell input
+    :param weights: list of float, whether there is a preference for some cells
     :return:
     """
 
-    batches = [generate_random_subset(inputs=inputs, num_cells_per_input=num_cells_per_input, batch_size=batch_size)
+    batches = [generate_random_subset(inputs=inputs, num_cells_per_input=num_cells_per_input,
+                                      batch_size=batch_size, weights=weights, return_indices=False)
                for _ in range(num_batches)]
     return batches
 
 
-def pca_plot(out_dir, real_data, fake_data, it, real_subs, experts):
+def generate_random_subset(inputs, num_cells_per_input, batch_size, weights=None, return_indices=False):
 
-    subpops = np.unique(real_subs)
-    markers = np.arange(real_data.shape[-1])
+    """
+    Returns a random subset from input data of shape (batch_size, num_cells_per_input, num_markers)
+    :param inputs: numpy array, the input ndarray to sample from
+    :param num_cells_per_input: int, number of cells per multi-cell input
+    :param batch_size: int, batch size of the subset
+    :param weights: list of float, whether there is a preference for some cells
+    :param return_indices: bool, whether to return subset indices or not
+    :return:
+    """
 
-    ncol = markers.shape[0]
-    nrow = len(subpops)
+    num_cells_total = inputs.shape[0]
 
-    dirname = os.path.join(out_dir, str((it//100)+1))
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
+    if weights is not None:
+        indices = np.random.choice(num_cells_total, size=batch_size * num_cells_per_input, replace=True, p=weights)
 
-    unique_experts = np.unique(experts)
+    else:
+        indices = np.random.choice(num_cells_total, size=batch_size * num_cells_per_input, replace=True)
 
-    for expert in unique_experts:
+    subset = inputs[indices, ]
+    subset = np.reshape(subset, newshape=(batch_size, num_cells_per_input, -1))
 
-        filename = os.path.join(dirname, 'Expert_' + str(expert + 1) + '.png')
-        expert_data = fake_data[np.where(experts == expert)[0], :]
+    if return_indices:
+        return subset, indices
 
-        f, ax = plt.subplots(nrows=nrow, ncols=ncol, figsize=(25, 25))
-        best_sub = None
-        best_KS_sum = np.inf
-
-        for sub in subpops:
-
-            KS_markers = list()
-
-            indices = np.where(real_subs == sub)[0]
-            sub_data = real_data[indices, :]
-
-            for marker in markers:
-
-                KS = ks_2samp(sub_data[:, marker], expert_data[:, marker])[0]
-                KS_markers.append(KS)
-
-                bins = np.linspace(sub_data[:, marker].min(), sub_data[:, marker].max(), num=30)
-                ax[sub, marker].hist(x=sub_data[:, marker], bins=bins, label='R', alpha=0.5)
-                ax[sub, marker].hist(x=expert_data[:, marker], bins=bins, label='F', alpha=0.5)
-                ax[sub, marker].set_title('Sub {0}, Mark {1}, KS {2:.2f}'.format(sub + 1, marker + 1, KS))
-                ax[sub, marker].legend()
-
-            if np.sum(KS_markers) <= best_KS_sum:
-                best_KS_sum = np.sum(KS_markers)
-                best_sub = sub
-
-        for marker in markers:
-
-            ax[best_sub, marker].spines['bottom'].set_color('0.0')
-            ax[best_sub, marker].spines['top'].set_color('0.0')
-            ax[best_sub, marker].spines['right'].set_color('0.0')
-            ax[best_sub, marker].spines['left'].set_color('0.0')
-            [i.set_linewidth(2.5) for i in ax[best_sub, marker].spines.values()]
-
-        f.tight_layout()
-        plt.savefig(filename)
-        plt.close()
+    else:
+        return subset
 
 
 def write_hparams_to_file(out_dir, hparams):
