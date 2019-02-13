bd7bcf4409da5b58750d3b7d683dfa637ee977c6
Vignesh <vsomnath@student.ethz.ch>
2019-02-13 14:19:39 +0100

    Saving expert-assignments

--- experiments/bodenmiller/run_bodenmiller.py
+++ experiments/bodenmiller/run_bodenmiller.py
@@ -502,9 +502,6 @@ def main():
                                                    fake_data=fake_samples, expert_labels=fake_sample_experts,
                                                    num_experts=num_experts, num_subpopulations=num_subpopulations)
 
-                with open(os.path.join(output_dir, str(iteration+1), 'expert_assignment.csv')) as f:
-                    f.write(json.dumps(expert_assignments))
-
                 # Compute learnt subpopulation weights
                 learnt_subpopulation_weights = \
                     compute_learnt_subpopulation_weights(expert_labels=fake_sample_experts,
@@ -574,6 +571,9 @@ def main():
                     iteration=iteration,
                     logger=cellgan_logger,
                     zero_sub=True)
+
+                with open(os.path.join(output_dir, str(iteration+1), 'expert_assignment.csv')) as f:
+                    f.write(json.dumps(expert_assignments))
 
                 # Umap on fake data
                 um2 = umap.UMAP(n_neighbors=5, min_dist=0.3, metric='correlation')
