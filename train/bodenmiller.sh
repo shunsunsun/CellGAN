8bf7a0e4bdd722b10de8a39c4c05980083d4fe5a
Vignesh <vsomnath@student.ethz.ch>
2019-02-15 17:41:06 +0100

    Fixed formatting

--- train/bodenmiller.sh
+++ train/bodenmiller.sh
@@ -4,8 +4,8 @@ module load python_gpu/3.6.4 cuda/9.0.176 cudnn/7.0
 
 echo "Training CellGAN on real data..."
 python -m cellgan.experiments.bodenmiller.run_bodenmiller \
-	--inhibitor AKTi \
-	--strength A02 \
+    --inhibitor AKTi \
+    --strength A02 \
     --disc_learning_rate 5e-4 \
     --gen_learning_rate 2e-4 \
     --experts 50 \
--- train/gmm.sh
+++ train/gmm.sh
@@ -4,8 +4,8 @@ module load python_gpu/3.6.4 cuda/9.0.176 cudnn/7.0
 
 echo "Training Gaussian Mixture Model on real data..."
 python -m cellgan.experiments.baselines.GMM.run_GMM \
-	--inhibitor AKTi \
-	--strength A02 \
+    --inhibitor AKTi \
+    --strength A02 \
     --learning_rate 5e-4 \
     --experts 20 \
     --num_cell_cnns 50 \
