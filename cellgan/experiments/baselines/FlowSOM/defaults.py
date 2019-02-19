6fac1542b2f53a2b00ff720c35336305b31f099e
Vignesh <vsomnath@student.ethz.ch>
2019-02-19 23:04:31 +0100

    Added defaults sinc evaluate_flowsom uses it

---
+++ cellgan/experiments/baselines/FlowSOM/defaults.py
@@ -0,0 +1,9 @@
+import os
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../'))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+# Setting up defaults
+DEFAULT_INHIBITOR = 'AKTi'
+DEFAULT_INHIB_STRENGTH = 'A02'
+DEFAULT_MARKERS = ['CD3', 'CD45', 'CD4', 'CD20', 'CD33', 'CD123', 'CD14', 'IgM', 'HLA-DR','CD7']
+DEFAULT_OUT_DIR = os.path.join(ROOT_DIR, 'results/baselines/FlowSOM')
