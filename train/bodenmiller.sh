ca479c19f14959d5e70e68115431b0b363d48ea4
Vignesh Ram Somnath <vsomnath@lo-login-01.leonhard.ethz.ch>
2019-03-13 10:10:06 +0100

    Modified train script

--- train/bodenmiller.sh
+++ train/bodenmiller.sh
@@ -5,11 +5,11 @@ module load python_gpu/3.6.4 cuda/9.0.176 cudnn/7.0
 echo "Training CellGAN on real data..."
 python -m cellgan.experiments.bodenmiller.run_bodenmiller \
     --inhibitor AKTi \
-    --strength A02 \
+    --strength A06 \
     --disc_learning_rate 5e-4 \
     --gen_learning_rate 2e-4 \
-    --experts 50 \
-    --num_cell_cnns 50 \
+    --experts 30 \
+    --num_cell_cnns 60 \
     --num_iter 10000 \
     --batch_size 64 \
     --noise_size 200 \
